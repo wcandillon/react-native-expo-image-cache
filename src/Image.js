391a1a9d51846221e19b21ca3ac4f3bf964ef810
William Candillon <wcandillon@gmail.com>
2018-04-06 18:45:38 +0200

    💚

--- src/Image.js
+++ src/Image.js
@@ -3,13 +3,13 @@ import * as _ from "lodash";
 import * as React from "react";
 import {Image as RNImage, Animated, StyleSheet, View, Platform} from "react-native";
 import {BlurView} from "expo";
-import {type StyleObj} from "react-native/Libraries/StyleSheet/StyleSheetTypes";
+import {type ImageStyle} from "react-native/Libraries/StyleSheet/StyleSheetTypes";
 import type {ImageSourcePropType} from "react-native/Libraries/Image/ImageSourcePropType";
 
 import CacheManager from "./CacheManager";
 
 type ImageProps = {
-    style?: StyleObj,
+    style?: ImageStyle,
     defaultSource?: ImageSourcePropType,
     preview?: ImageSourcePropType,
     uri: string
